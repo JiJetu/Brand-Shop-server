1ca107f74f64d0f7d4617dee22fb1968accdce89
JiJetu <jijetu2@gmail.com>
2023-10-24 00:19:00 +0600

    brands are connected to db

--- index.js
+++ index.js
@@ -27,12 +27,49 @@ async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
+
+        
+        const brandCollection = client.db("brandDB").collection("brand");
+
+        app.get('/brand', async (req, res) => {
+
+            const query = {};
+            const options = {};
+            const cursor = await brandCollection.find(query, options).toArray();
+
+            res.send(cursor)
+        })
+
+        app.post('/brand', async (req, res) => {
+            const data = req.body;
+
+            const brands = await brandCollection.insertMany(data)
+            res.send(brands)
+        })
+
+        
+
+        const productCollection = client.db('productDB').collection('product')
+
+        app.get('/product', async (req, res) => {
+            const cursor = productCollection.find();
+            const result = await cursor.toArray();
+            res.send(result);
+        })
+
+        app.post('/product', async (req, res) => {
+            const newProduct = req.body;
+            console.log(newProduct);
+            const result = await productCollection.insertOne(newProduct);
+            res.send(result);
+        })
+
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
-        await client.close();
+        // await client.close();
     }
 }
 run().catch(console.dir);
